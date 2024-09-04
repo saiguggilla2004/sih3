bc16e966bfd875949f9e97bc95fc123f159b55c2
sai guggilla <146122498+saiguggilla2004@users.noreply.github.com>
2024-09-04 12:02:40 +0530

    Add files via upload

---
+++ models/event.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  attendees: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Event', EventSchema);
